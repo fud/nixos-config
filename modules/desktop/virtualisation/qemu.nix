62bc2723cdfc6785412dbca0dc904d2759074c86
Brenton Bills <Brenton.Bills@gmail.com>
2022-10-07 17:20:35 +1000

    Turning this off for now

--- modules/desktop/virtualisation/qemu.nix
+++ modules/desktop/virtualisation/qemu.nix
@@ -39,11 +39,11 @@
 
   boot.kernelParams = [ "pci=noats" "amd_iommu=on" "iommu=pt" ];
 
-  boot.initrd.preDeviceCommands = ''
-        DEVS="06:00.0 06:00.1"
-        for DEV in $DEVS; do
-            echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
-        done
-        modprobe -i vfio-pci
-  '';
+  # boot.initrd.preDeviceCommands = ''
+  #       DEVS="06:00.0 06:00.1"
+  #       for DEV in $DEVS; do
+  #           echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+  #       done
+  #       modprobe -i vfio-pci
+  # '';
 }
